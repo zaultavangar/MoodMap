d8267c25ad7e01b122e3f955d950e0d62ef27556
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-11-13 13:56:58 -0500

    feat(frontend): Added vitest for unit testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { ViteAliases } from "vite-aliases";
@@ -5,4 +7,7 @@ import { ViteAliases } from "vite-aliases";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ViteAliases()],
+  test: {
+    include: ["tests/unit/*.test.ts"],
+  },
 });
