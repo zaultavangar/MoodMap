96895b8c7af2a37318623bcd5d9fb00a9be97d57
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-11-13 14:46:01 -0500

    feat(frontend): Added basic Searchbar component

---
+++ frontend/src/components/Searchbar.tsx
@@ -0,0 +1,47 @@
+import { Paper, InputBase, IconButton } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+
+const MenuButton = () => {
+  return (
+    <IconButton type="button" aria-label="menu">
+      <MenuIcon />
+    </IconButton>
+  );
+};
+
+const SearchButton = () => {
+  return (
+    <IconButton type="button" aria-label="search">
+      <SearchIcon />
+    </IconButton>
+  );
+};
+
+/**
+ * Searchbar component allows users to search news articles based on a given keyword
+ */
+const Searchbar = () => {
+  return (
+    <Paper
+      component="form"
+      elevation={2}
+      sx={{ p: "2px 4px", display: "flex", alignItems: "center" }}
+      data-testid="searchbar"
+    >
+      <MenuButton />
+      {/* Input field to search for keyword */}
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        id="outlined-basic"
+        placeholder="Search the news by keyword"
+        inputProps={{ "aria-label": "search the news by keyword" }}
+        // value={searchQuery}
+        // onChange={handleSearchChange}
+      />
+      <SearchButton />
+    </Paper>
+  );
+};
+
+export default Searchbar;
