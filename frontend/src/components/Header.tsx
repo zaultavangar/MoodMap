cef10f188dd1b98a4a97447a1e32fd69b8e06c31
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-11-13 15:55:00 -0500

    feat(frontend): Created SearchResults component to see keyword results

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,28 @@
 import Grid from "@mui/material/Unstable_Grid2";
 import Searchbar from "./Searchbar";
+import SearchResults from "./SearchResults";
+import { useEffect, useState } from "react";
+import { keywords } from "~/data/keywords";
 const Header = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [results, setResults] = useState<string[]>([]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    const foundKeywords = keywords.filter(
+      (keyword) => keyword.toLowerCase() === searchQuery.toLowerCase()
+    );
+    setResults(foundKeywords);
+  }, [searchQuery]);
+
   return (
     <Grid
       container
       component="header"
+      spacing={2}
       sx={{
         zIndex: 4,
         position: "relative",
@@ -13,8 +31,13 @@ const Header = () => {
       }}
     >
       <Grid xs={12} md={6}>
-        <Searchbar />
+        <Searchbar searchQuery={searchQuery} onChange={handleSearchChange} />
       </Grid>
+      {results.length > 0 && (
+        <Grid xs={12} md={6}>
+          <SearchResults results={results} />
+        </Grid>
+      )}
     </Grid>
   );
 };
