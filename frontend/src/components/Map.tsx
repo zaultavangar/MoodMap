e7c0e7126995f9b8126a02de4883b36caef3a7c7
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-11-13 14:32:46 -0500

    feat(frontend): Added basic Map component with custom map view hook

---
+++ frontend/src/components/Map.tsx
@@ -0,0 +1,30 @@
+import { Map as ReactMap } from "react-map-gl";
+import { useMapView } from "~/hooks/useMapView";
+// Accesing the mapbox API token
+const MAPBOX_API_TOKEN = import.meta.env.VITE_MAPBOX_API_TOKEN;
+
+const Map = () => {
+  const { mapViewState, handleMapMove } = useMapView();
+  return (
+    <ReactMap
+      mapboxAccessToken={MAPBOX_API_TOKEN}
+      {...mapViewState}
+      // mapStyle="mapbox://styles/mapbox/streets-v12"
+      mapStyle="mapbox://styles/mapbox/streets-v12"
+      //   interactiveLayerIds={["unfiltered-data"]}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        zIndex: 0,
+        width: "100vw",
+        height: "100vh",
+        overflow: "hidden",
+      }}
+      onMove={handleMapMove}
+    ></ReactMap>
+  );
+};
+
+export default Map;
---
+++ frontend/src/hooks/useMapView.ts
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import type { ViewStateChangeEvent } from "react-map-gl";
+
+// Default location of the map
+const ProvidenceLatLong = {
+  long: -71.4141362441059,
+  lat: 41.82454500035089,
+};
+
+export function useMapView({
+  long,
+  lat,
+}: { long: number; lat: number } = ProvidenceLatLong) {
+  const [mapViewState, setMapViewState] = useState({
+    longitude: long,
+    latitude: lat,
+    zoom: 12,
+    // Changes the tilt of the map
+    // pitch: 60,
+  });
+
+  // Handling the map move
+  const handleMapMove = (e: ViewStateChangeEvent) => {
+    setMapViewState(e.viewState);
+  };
+
+  return {
+    mapViewState,
+    handleMapMove,
+  };
+}
