12e167eedb69177c05fc71d76b4ed04fa90a9efa
SunZh <zhenhao_sun@brown.edu>
2023-11-22 14:54:01 +0800

    feat: implement scheduled task to automatically extract article information

---
+++ backend/src/main/java/com/example/backend/scheduledTask/ScheduledArticleTask.java
@@ -0,0 +1,21 @@
+package com.example.backend.scheduledTask;
+
+import com.example.backend.processor.Processor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+@Component
+public class ScheduledArticleTask {
+
+    @Resource
+    private Processor processor;
+
+    @Scheduled(cron = "0 0 5 * * *", zone = "America/New_York")
+    public void scheduledTask() {
+        System.out.println("Running scheduled task at 5 AM EST");
+        // Your task logic here
+        processor.processArticles("2023-11-20", "2023-11-21");
+    }
+}
