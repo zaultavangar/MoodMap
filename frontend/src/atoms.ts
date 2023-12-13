2cd131f642810619cebb267b93b4345ec2189617
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-12-12 23:07:37 -0500

    feat(frontend): Added keywords as a default so that search results could be viewed

--- frontend/src/atoms.ts
+++ frontend/src/atoms.ts
@@ -2,66 +2,71 @@ import { RecoilState, atom } from "recoil";
 import { LocationPopupInfo } from "./hooks/useLocationPopup";
 import { CircleLayer } from "mapbox-gl";
 import { circleLayerr } from "./components/heatmapLayer";
-import { Feature, FeatureCollection, GeoJsonProperties, Geometry } from "geojson";
+import {
+  Feature,
+  FeatureCollection,
+  GeoJsonProperties,
+  Geometry,
+} from "geojson";
+import { keywords } from "./data/keywords";
 
 export const searchQueryState: RecoilState<string> = atom({
-  key: 'searchQuery',
-  default: ''
-})
+  key: "searchQuery",
+  default: "",
+});
 
 export const searchResultsState: RecoilState<string[]> = atom({
-  key: 'searchResults',
-  default: [] as string[]
-})
+  key: "searchResults",
+  default: keywords as string[],
+});
 
-export const searchDateFilterPopoverAnchorElState: RecoilState<HTMLButtonElement | null> = atom({
-  key: 'searchDateFilterPopoverAnchorEl',
-  default: null as HTMLButtonElement | null
-})
+export const searchDateFilterPopoverAnchorElState: RecoilState<HTMLButtonElement | null> =
+  atom({
+    key: "searchDateFilterPopoverAnchorEl",
+    default: null as HTMLButtonElement | null,
+  });
 
 export const selectedDateRangeState: RecoilState<string> = atom({
-  key: 'selectedDateRangeState',
-  default: '12-2023'
-})
+  key: "selectedDateRangeState",
+  default: "12-2023",
+});
 
 export const isFullCalendarState: RecoilState<boolean> = atom({
-  key: 'isFullCalendar',
-  default: true
-})
+  key: "isFullCalendar",
+  default: true,
+});
 
 export const isExpandedStatsOverviewState: RecoilState<boolean> = atom({
-  key: 'isExpandedStatsOverview',
-  default: true
-})
+  key: "isExpandedStatsOverview",
+  default: true,
+});
 
-export const locationPopupInfoState: RecoilState<LocationPopupInfo | null> = atom({
-  key: 'locationPopupInfo',
-  default: null as LocationPopupInfo | null
-})
+export const locationPopupInfoState: RecoilState<LocationPopupInfo | null> =
+  atom({
+    key: "locationPopupInfo",
+    default: null as LocationPopupInfo | null,
+  });
 
 export const circleLayerState: RecoilState<CircleLayer> = atom({
-  key: 'circleLayer',
-  default: circleLayerr
-})
+  key: "circleLayer",
+  default: circleLayerr,
+});
 
 export const mapViewStateState: RecoilState<any> = atom({
-  key: 'mapViewState',
+  key: "mapViewState",
   default: {
     longitude: 34.37,
     latitude: 31.5,
-    zoom: 1
-  }
-})
+    zoom: 1,
+  },
+});
 
-export const mapFeatureCollectionState: RecoilState<FeatureCollection<Geometry, GeoJsonProperties>> = atom({
-  key: 'mapFeatureCollection',
+export const mapFeatureCollectionState: RecoilState<
+  FeatureCollection<Geometry, GeoJsonProperties>
+> = atom({
+  key: "mapFeatureCollection",
   default: {
-    type: 'FeatureCollection',
-    features: [] as Array<Feature<Geometry, GeoJsonProperties>>
-  }
-})
-
-
-
-
-
+    type: "FeatureCollection",
+    features: [] as Array<Feature<Geometry, GeoJsonProperties>>,
+  },
+});
