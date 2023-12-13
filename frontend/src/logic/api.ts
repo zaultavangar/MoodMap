f16497395b825d9c56326156321a8223bc1aa984
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-12-12 23:09:54 -0500

    feat(frontend): started chaging api so that it was class-based and could mock responses

--- frontend/src/logic/api.ts
+++ frontend/src/logic/api.ts
@@ -1,22 +1,22 @@
-import axios, { AxiosResponse } from 'axios';
-import { GeoJsonProperties, Geometry } from 'geojson';
+import axios, { AxiosResponse } from "axios";
+import { GeoJsonProperties, Geometry } from "geojson";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export type ArticleEntity = {
-  webTitle: string
-  webUrl: string,
-  sentimentScore: number
-  associatedLocation: string[]
-  thumbnail: string
+  webTitle: string;
+  webUrl: string;
+  sentimentScore: number;
+  associatedLocation: string[];
+  thumbnail: string;
   //url: string;
 };
 
 export type FeatureEntity = {
-  type: 'Feature';
-  geometry: Geometry
-  properties: GeoJsonProperties
-}
+  type: "Feature";
+  geometry: Geometry;
+  properties: GeoJsonProperties;
+};
 
 export type ApiResponse<T> = {
   timestamp: string;
@@ -74,22 +74,96 @@ type EndPoints = {
 
 export type Endpoint = keyof EndPoints;
 
+interface API {
+  getFeatures(): Promise<FrontendApiResponse<FeatureEntity[]>>;
+  searchByLocation(
+    location: string,
+    fromDate: string,
+    toDate: string
+  ): Promise<FrontendApiResponse<ArticleEntity[]>>;
+}
+
+class MockAPI implements API {
+  getFeatures(): Promise<FrontendApiResponse<FeatureEntity[]>> {
+    throw new Error("Method not implemented.");
+  }
+  searchByLocation(
+    location: string,
+    fromDate: string,
+    toDate: string
+  ): Promise<FrontendApiResponse<ArticleEntity[]>> {
+    throw new Error("Method not implemented.");
+  }
+}
+
+class RealAPI implements API {
+  getFeatures(): Promise<FrontendApiResponse<FeatureEntity[]>> {
+    return axios
+      .get(`${API_BASE_URL}/getFeatures`, {
+        validateStatus: (status) => status !== 500 && status !== 400,
+      })
+      .then((response) => {
+        console.error("Response", response);
+        return {
+          data: response.data.data,
+        };
+      })
+      .catch((error) =>
+        error.response
+          ? {
+              message: error.response.data.result,
+            }
+          : {
+              message: "Unknown error",
+            }
+      );
+  }
+  searchByLocation(
+    location: string,
+    fromDate: string,
+    toDate: string
+  ): Promise<FrontendApiResponse<ArticleEntity[]>> {
+    return axios
+      .get(`${API_BASE_URL}/searchByLocation`, {
+        params: { location, fromDate, toDate },
+        validateStatus: (status) => status !== 500 && status !== 400,
+      })
+      .then((response) => {
+        console.error("Response", response);
+        return {
+          data: response.data.data,
+        };
+      })
+      .catch((error) =>
+        error.response
+          ? {
+              message: error.response.data.result,
+            }
+          : {
+              message: "Unknown error",
+            }
+      );
+  }
+}
+
 export const handleApiResponse = async <T extends Endpoint, K>(
   endpoint: T,
   queryParams: EndPoints[T]
 ): Promise<FrontendApiResponse<K>> => {
   return axios
-    .get<AxiosResponse<ApiResponse<K>>, AxiosResponse<ApiResponse<K>>>(`${API_BASE_URL}/${endpoint}`, {
-      params: { ...queryParams },
-      validateStatus: (status) => status !== 500 && status !== 400,
-    })
+    .get<AxiosResponse<ApiResponse<K>>, AxiosResponse<ApiResponse<K>>>(
+      `${API_BASE_URL}/${endpoint}`,
+      {
+        params: { ...queryParams },
+        validateStatus: (status) => status !== 500 && status !== 400,
+      }
+    )
     .then((response) => {
-      console.error("Response", response)
+      console.error("Response", response);
       return {
-      
-      data: response.data.data
-    
-    }})
+        data: response.data.data,
+      };
+    })
     .catch((error) =>
       error.response
         ? {
@@ -101,6 +175,8 @@ export const handleApiResponse = async <T extends Endpoint, K>(
     );
 };
 
-export const isSuccessfulResponse = <T>(response: FrontendApiResponse<T>): response is { data: T } => {
-  return 'data' in response;
+export const isSuccessfulResponse = <T>(
+  response: FrontendApiResponse<T>
+): response is { data: T } => {
+  return "data" in response;
 };
