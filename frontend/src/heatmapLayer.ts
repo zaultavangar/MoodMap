a2a95b90f1feb8d705c28a45ee9437083f2f86e7
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-12-17 22:12:37 -0500

    refactor(frontend): prettier linting

--- frontend/src/heatmapLayer.ts
+++ frontend/src/heatmapLayer.ts
@@ -3,93 +3,138 @@ import type { CircleLayer } from "mapbox-gl";
 
 const getMaxArticleCount = (countKey: string, features: Feature[]): number => {
   const counts: number[] = features.map((feature, index) => {
-    
-    if (feature.properties !== null && feature.properties[countKey] !== null && feature.properties[countKey] !== undefined){
+    if (
+      feature.properties !== null &&
+      feature.properties[countKey] !== null &&
+      feature.properties[countKey] !== undefined
+    ) {
       return feature.properties[countKey];
     } else {
       return 0; // Return 0 if the property is null or undefined
     }
   });
-  return Math.max(...counts)
-}
+  return Math.max(...counts);
+};
 
-export const updateHeatMapLayer = (features: Feature[], year: number, month?: number): CircleLayer | null => {
-  const formattedMonth = month ? month.toString().padStart(2, '0') : '';
+export const updateHeatMapLayer = (
+  features: Feature[],
+  year: number,
+  month?: number
+): CircleLayer | null => {
+  const formattedMonth = month ? month.toString().padStart(2, "0") : "";
   const formattedYear = year.toString();
-  const sentimentKey: string = (formattedMonth ? `${formattedMonth}-${formattedYear}` : formattedYear) + '-sentiment';
-  const countKey: string = (formattedMonth ? `${formattedMonth}-${formattedYear}` : formattedYear) + '-count';
+  const sentimentKey: string =
+    (formattedMonth ? `${formattedMonth}-${formattedYear}` : formattedYear) +
+    "-sentiment";
+  const countKey: string =
+    (formattedMonth ? `${formattedMonth}-${formattedYear}` : formattedYear) +
+    "-count";
 
   const maxCount = getMaxArticleCount(countKey, features);
   if (maxCount > 10 && maxCount <= 15) return null;
   const circleLayer: CircleLayer = {
-    id: 'heatmap',
-    type: 'circle',
-    filter: ['!=', ['typeof', ['get', countKey]], 'null'], // excludes points that have no articles for that month
+    id: "heatmap",
+    type: "circle",
+    filter: ["!=", ["typeof", ["get", countKey]], "null"], // excludes points that have no articles for that month
     paint: {
-        // circle radius based on total number of articles
-        'circle-radius': [
-          'interpolate',
-          ['linear'],
-          ['zoom'],
-          0, ['step', ['get', countKey],
-          3, 5,
-          10, 10,
-          20, 15,
-          30, 80,
-          20 // default value
-        ],
-        8, ['step', ['get', countKey],
-          5, 10,
-          10, 12,
-          20, 13,
-          30, 15,
-          20 // default value
-        ]
-        ],
-        // color of the circle based on sentiment
-        'circle-color': 
+      // circle radius based on total number of articles
+      "circle-radius": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
+        0,
         [
-            'interpolate',
-            ['linear'],
-            ['get', sentimentKey],
-            0.2, "#ff0000",       // Low sentiment
-            0.7, "#00ff00"     // High sentiment
+          "step",
+          ["get", countKey],
+          3,
+          5,
+          10,
+          10,
+          20,
+          15,
+          30,
+          80,
+          20, // default value
         ],
-        'circle-opacity': 0.75
-    }
+        8,
+        [
+          "step",
+          ["get", countKey],
+          5,
+          10,
+          10,
+          12,
+          20,
+          13,
+          30,
+          15,
+          20, // default value
+        ],
+      ],
+      // color of the circle based on sentiment
+      "circle-color": [
+        "interpolate",
+        ["linear"],
+        ["get", sentimentKey],
+        0.2,
+        "#ff0000", // Low sentiment
+        0.7,
+        "#00ff00", // High sentiment
+      ],
+      "circle-opacity": 0.75,
+    },
   };
   return circleLayer;
-}
+};
 
 export const circleLayerr: CircleLayer = {
-  id: 'heatmap',
-    type: 'circle',
-    paint: {
-        // Size of the circle
-        'circle-radius': [
-          'interpolate',
-          ['linear'],
-          ['zoom'],
-          7, 
-          ['interpolate', ['exponential', 5], ['get', "11-2023-count"], 1, 4, 150, 12],
-          16, 
-          ['interpolate', ['exponential', 5], ['get', "11-2023-count"], 1, 8, 150, 24]
-        ],
+  id: "heatmap",
+  type: "circle",
+  paint: {
+    // Size of the circle
+    "circle-radius": [
+      "interpolate",
+      ["linear"],
+      ["zoom"],
+      7,
+      [
+        "interpolate",
+        ["exponential", 5],
+        ["get", "11-2023-count"],
+        1,
+        4,
+        150,
+        12,
+      ],
+      16,
+      [
+        "interpolate",
+        ["exponential", 5],
+        ["get", "11-2023-count"],
+        1,
+        8,
+        150,
+        24,
+      ],
+    ],
 
-        // Color of the circle based on sentiment
-        'circle-color': [
-            'interpolate',
-            ['linear'],
-            ['get', "2023-11-sentiment"],
-            0, "#ff0000",       // Low sentiment
-            0.5, "#ffa500",  // Medium sentiment
-            1, "#ffff00"     // High sentiment
-        ],
+    // Color of the circle based on sentiment
+    "circle-color": [
+      "interpolate",
+      ["linear"],
+      ["get", "2023-11-sentiment"],
+      0,
+      "#ff0000", // Low sentiment
+      0.5,
+      "#ffa500", // Medium sentiment
+      1,
+      "#ffff00", // High sentiment
+    ],
 
-        // Optional: Adjust the circle opacity
-        'circle-opacity': 0.75
-    }
-}
+    // Optional: Adjust the circle opacity
+    "circle-opacity": 0.75,
+  },
+};
 
 // export const heatmapLayer: HeatmapLayer = {
 //   id: "heatmap",
@@ -117,7 +162,7 @@ export const circleLayerr: CircleLayer = {
 //       MAX_ZOOM_LEVEL,
 //       3,
 //     ],
-    
+
 //     // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
 //     // Begin color ramp at 0-stop with a 0-transparancy color
 //     // to create a blur-like effect.
