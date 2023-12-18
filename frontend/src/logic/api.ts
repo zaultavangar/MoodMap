078c076dc386ec7af1caa0acb39e615eac0e84a7
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-12-17 22:16:02 -0500

    feat(frontend): Refactored so that mock data and real data could be used in frontend

--- frontend/src/logic/api.ts
+++ frontend/src/logic/api.ts
@@ -1,8 +1,14 @@
 import axios, { AxiosResponse } from "axios";
 import { ArticleEntity, FeatureEntity } from "~/types";
-
+//@ts-ignore
+import mockFeaturesResponse from "../../__mocks__/features.json";
+//@ts-ignore
+import mockLocationResponse from "../../__mocks__/location.json";
+//@ts-ignore
+import mockDateRangeResponse from "../../__mocks__/dateRange.json";
+//@ts-ignore
+import mockKeywordResponse from "../../__mocks__/keyword.json";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 
 export type ApiResponse<T> = {
   timestamp: string;
@@ -62,6 +68,11 @@ export type Endpoint = keyof EndPoints;
 
 interface API {
   getFeatures(): Promise<FrontendApiResponse<FeatureEntity[]>>;
+  search(
+    input: string,
+    fromDate?: string,
+    toDate?: string
+  ): Promise<FrontendApiResponse<ArticleEntity[]>>;
   searchByLocation(
     location: string,
     fromDate: string,
@@ -70,34 +81,73 @@ interface API {
 }
 
 class MockAPI implements API {
+  search(
+    input: string,
+    fromDate?: string,
+    toDate?: string
+  ): Promise<FrontendApiResponse<ArticleEntity[]>> {
+    return Promise.resolve({
+      data: mockKeywordResponse.data,
+    });
+  }
   getFeatures(): Promise<FrontendApiResponse<FeatureEntity[]>> {
-    throw new Error("Method not implemented.");
+    return Promise.resolve({
+      data: mockFeaturesResponse.data,
+    });
   }
   searchByLocation(
     location: string,
     fromDate: string,
     toDate: string
   ): Promise<FrontendApiResponse<ArticleEntity[]>> {
-    throw new Error("Method not implemented.");
+    return Promise.resolve({
+      data: mockLocationResponse.data,
+    });
   }
 }
 
-class RealAPI implements API {
+export class RealAPI implements API {
+  search(
+    input: string,
+    fromDate?: string,
+    toDate?: string
+  ): Promise<FrontendApiResponse<ArticleEntity[]>> {
+    return axios
+      .get(`${API_BASE_URL}/search`, {
+        params: { input, fromDate, toDate },
+        validateStatus: (status) => status !== 500 && status !== 400,
+      })
+      .then((response) => {
+        // console.error("Response", response);
+        return {
+          data: response.data.data,
+        };
+      })
+      .catch((error) =>
+        error
+          ? {
+              message: error.result,
+            }
+          : {
+              message: "Unknown error",
+            }
+      );
+  }
   getFeatures(): Promise<FrontendApiResponse<FeatureEntity[]>> {
     return axios
       .get(`${API_BASE_URL}/getFeatures`, {
         validateStatus: (status) => status !== 500 && status !== 400,
       })
       .then((response) => {
-        console.error("Response", response);
+        // console.error("Response", response);
         return {
           data: response.data.data,
         };
       })
       .catch((error) =>
-        error.response
+        error
           ? {
-              message: error.response.data.result,
+              message: error.result,
             }
           : {
               message: "Unknown error",
@@ -115,15 +165,15 @@ class RealAPI implements API {
         validateStatus: (status) => status !== 500 && status !== 400,
       })
       .then((response) => {
-        console.error("Response", response);
+        // console.error("Response", response);
         return {
-          data: response.data.data,
+          data: response.data,
         };
       })
       .catch((error) =>
-        error.response
+        error
           ? {
-              message: error.response.data.result,
+              message: error.result,
             }
           : {
               message: "Unknown error",
@@ -132,37 +182,42 @@ class RealAPI implements API {
   }
 }
 
-export const handleApiResponse = async <T extends Endpoint, K>(
-  endpoint: T,
-  queryParams: EndPoints[T]
-): Promise<FrontendApiResponse<K>> => {
-  return axios
-    .get<AxiosResponse<ApiResponse<K>>, AxiosResponse<ApiResponse<K>>>(
-      `${API_BASE_URL}/${endpoint}`,
-      {
-        params: { ...queryParams },
-        validateStatus: (status) => status !== 500 && status !== 400,
-      }
-    )
-    .then((response) => {
-      console.error("Response", response);
-      return {
-        data: response.data.data,
-      };
-    })
-    .catch((error) =>
-      error.response
-        ? {
-            message: error.response.data.result,
-          }
-        : {
-            message: "Unknown error",
-          }
-    );
-};
+// export const handleApiResponse = async <T extends Endpoint, K>(
+//   endpoint: T,
+//   queryParams: EndPoints[T]
+// ): Promise<FrontendApiResponse<K>> => {
+//   return axios
+//     .get<AxiosResponse<ApiResponse<K>>, AxiosResponse<ApiResponse<K>>>(
+//       `${API_BASE_URL}/${endpoint}`,
+//       {
+//         params: { ...queryParams },
+//         validateStatus: (status) => status !== 500 && status !== 400,
+//       }
+//     )
+//     .then((response) => {
+//       console.error("Response", response);
+//       return {
+//         data: response.data.data,
+//       };
+//     })
+//     .catch((error) =>
+//       error.response
+//         ? {
+//             message: error.response.data.result,
+//           }
+//         : {
+//             message: "Unknown error",
+//           }
+//     );
+// };
 
 export const isSuccessfulResponse = <T>(
   response: FrontendApiResponse<T>
 ): response is { data: T } => {
   return "data" in response;
 };
+
+// const api = import.meta.env.MODE === "dev" ? new MockAPI() : new RealAPI();
+const api = new RealAPI();
+
+export default api;
