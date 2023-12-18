602e7d26a98e73a16e84d3b795518ee31f81c113
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-12-17 22:14:15 -0500

    test(frontend): Added testing labels for e2e tests

--- frontend/src/components/SearchResults.tsx
+++ frontend/src/components/SearchResults.tsx
@@ -10,62 +10,83 @@ import {
   Stack,
 } from "@mui/material";
 import NewspaperIcon from "@mui/icons-material/Newspaper";
-import CircleIcon from '@mui/icons-material/Circle';
+import CircleIcon from "@mui/icons-material/Circle";
 import { useRecoilValue } from "recoil";
-import { searchByDateRangeState, searchQueryState, searchResultsState, selectedMonthState, selectedYearState  } from "~/atoms";
+import {
+  searchByDateRangeState,
+  searchQueryState,
+  searchResultsState,
+  selectedMonthState,
+  selectedYearState,
+} from "~/atoms";
 import { useSearch } from "~/hooks/useSearch";
 import * as d3 from "d3";
 
 const SearchResults = () => {
-
   const searchQuery = useRecoilValue(searchQueryState);
   const searchResults = useRecoilValue(searchResultsState);
   const selectedMonth = useRecoilValue(selectedMonthState);
   const selectedYear = useRecoilValue(selectedYearState);
   const searchByDateRange = useRecoilValue(searchByDateRangeState);
 
-  const {search} = useSearch();
+  const { search } = useSearch();
 
   useEffect(() => {
     search(searchQuery);
   }, [searchQuery]);
 
   useEffect(() => {
-    if (searchQuery.length > 0){
+    if (searchQuery.length > 0) {
       search(searchQuery);
     }
-  }, [selectedMonth, selectedYear, searchByDateRange])
+  }, [selectedMonth, selectedYear, searchByDateRange]);
 
   const interpolater = d3.interpolateRgbBasis(["red", "yellow", "green"]);
 
   return (
-    <Paper elevation={2} style={{maxHeight: '80vh', overflow: 'scroll'}}>
+    <Paper
+      elevation={2}
+      style={{ maxHeight: "80vh", overflow: "scroll" }}
+      data-testid="search-results"
+    >
       <List dense={false}>
         {searchResults.map((result, index) => (
-          <Link href={result.webUrl} target="_blank" underline="none" color={'inherit'}>
+          <Link
+            href={result.webUrl}
+            target="_blank"
+            underline="none"
+            color={"inherit"}
+          >
             <ListItemButton key={index}>
               <ListItemIcon>
                 <NewspaperIcon />
               </ListItemIcon>
-                <Stack
-                  alignItems={"center"}
-                  direction="row"
-                  spacing={2}
-                >
-                  <ListItemText 
-                    primary={result.webTitle} 
-                    secondary={
-                      <Stack
-                        direction='column'
-                        justifyContent={'center'}
-                        spacing={1}
-                        >
-                        {result.webPublicationDate.substring(0,10)} 
-                      </Stack> 
-                    }/>
-                  <CircleIcon style={{color: interpolater(result.sentimentScore), fontSize: '20px'}}/>
-                  <img src={result.thumbnail} alt='Article image' width='100px' style={{borderRadius: '5px'}}/>
-                </Stack>
+              <Stack alignItems={"center"} direction="row" spacing={2}>
+                <ListItemText
+                  primary={result.webTitle}
+                  secondary={
+                    <Stack
+                      direction="column"
+                      justifyContent={"center"}
+                      spacing={1}
+                    >
+                      {result.webPublicationDate.substring(0, 10)}
+                    </Stack>
+                  }
+                />
+                <CircleIcon
+                  style={{
+                    color: interpolater(result.sentimentScore),
+                    fontSize: "20px",
+                  }}
+                />
+                <img
+                  src={result.thumbnail}
+                  alt="Article image"
+                  width="100px"
+                  style={{ borderRadius: "5px" }}
+                />
+              </Stack>
             </ListItemButton>
           </Link>
         ))}
