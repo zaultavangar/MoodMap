65be53b586e42a15ab0598b039c52df1966d502b
okapi2k22 <nicolas_perez+cs1300@brown.edu>
2023-12-17 22:16:20 -0500

    test(frontend): e2e tests for components

--- frontend/tests/e2e/Searchbar.test.ts
+++ frontend/tests/e2e/Searchbar.test.ts
@@ -5,12 +5,32 @@ import { test, expect } from "@playwright/test";
  * a user can enter a keyword
  */
 test.describe("SearchBar component", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173/");
+  });
   test("should be able to see search bar and enter a keyword", async ({
     page,
   }) => {
-    await page.goto("http://localhost:5173/");
     await expect(page.getByTestId("searchbar")).toBeVisible();
-    await page.getByPlaceholder("Search an area by keyword").click();
-    await page.getByPlaceholder("Search an area by keyword").fill("Providence");
+  });
+  test("should be able to enter a keyword and see results", async ({
+    page,
+  }) => {
+    await page.getByTestId("searchbar-input").click();
+    await page.getByTestId("searchbar-input").fill("gaza");
+    await expect(page.getByTestId("search-results")).toBeVisible();
+  });
+  test("should be able to click on a date range button to toggle search options", async ({
+    page,
+  }) => {
+    const dateRangeButton = page.getByTestId("searchbar-date-range-button");
+    await expect(dateRangeButton).toBeVisible();
+    await dateRangeButton.click();
+
+    const dateRangeCheckbox = page.getByTestId(
+      "search-date-range-option-checkbox"
+    );
+    await expect(dateRangeCheckbox).toBeVisible();
+    await dateRangeCheckbox.click();
   });
 });
